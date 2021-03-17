f6cebe3afbb850dbd7a8559c9d5e62129c36da4a
Wesenheit <77942828+Wesenheit@users.noreply.github.com>
2021-03-17 15:05:42 +0100

    lepsze formatowanie

--- NumberTheory/core.py
+++ NumberTheory/core.py
@@ -1,64 +1,66 @@
 import math
 
 
-__all__=['sito','fact','tot','primes','nwd']
-
+__all__ = ['sito', 'fact', 'tot', 'primes', 'nwd']
 
 
 def sito(u):
-	tab=[0 for i in range(u+1)]
-	for i in range(2,u+1):
-		tab[i]=i
-	for i in range(4, u+1, 2):
-        	tab[i]=2
-	for i in range(3,math.floor(math.sqrt(u))):
-		if tab[i]==i:
-			for j in range(i*i,u+1,i):
-				if (tab[j]==j):
-					tab[j]=i
-	return tab
+    tab = [0 for i in range(u+1)]
+    for i in range(2, u+1):
+        tab[i] = i
+    for i in range(4, u+1, 2):
+        tab[i] = 2
+    for i in range(3, math.floor(math.sqrt(u))):
+        if tab[i] == i:
+            for j in range(i*i, u+1, i):
+                if (tab[j] == j):
+                    tab[j] = i
+    return tab
 
-def fact(x,licz):
-	primes=[]
-	counts=[]
-	primes.append(licz[x])
-	counts.append(1)
-	x = x//licz[x]
-	while x>1:
-		if licz[x] == primes[-1]:
-			counts[-1]+=1
-			x=x//licz[x]
-		else:
-			primes.append(licz[x])
-			counts.append(1)
-			x=x//licz[x]
-	wyn=[primes,counts]
-	return wyn
 
-def primes(x,licz):
-	primes=[]
-	while x>1:
-		if licz[x] in primes:
-			x=x//licz[x]
-		else:
-			primes.append(licz[x])
-			x=x//licz[x]
-	return primes
+def fact(x, licz):
+    primes = []
+    counts = []
+    primes.append(licz[x])
+    counts.append(1)
+    x = x // licz[x]
+    while x > 1:
+        if licz[x] == primes[-1]:
+            counts[-1] += 1
+            x = x // licz[x]
+        else:
+            primes.append(licz[x])
+            counts.append(1)
+            x = x // licz[x]
+    wyn = [primes, counts]
+    return wyn
 
-def tot(x,licz):
-	p=primes(x,licz)
-	if x == 0:
-		return 0
-	else:
-		wyn=1
-		for i in p:
-			wyn*=(1-1/i)
-		return int(wyn)
 
-def nwd(a,b):
-	while b > 0:
-		c = a % b
-		a = b
-		b = c
-	return a
-	
+def primes(x, licz):
+    primes = []
+    while x > 1:
+        if licz[x] in primes:
+            x = x // licz[x]
+        else:
+            primes.append(licz[x])
+            x = x // licz[x]
+    return primes
+
+
+def tot(x, licz):
+    p = primes(x, licz)
+    if x == 0:
+        return 0
+    else:
+        wyn = 1
+        for i in p:
+            wyn *= (1-1/i)
+        return int(wyn)
+
+
+def nwd(a, b):
+    while b > 0:
+        c = a % b
+        a = b
+        b = c
+    return a
--- NumberTheory/modulo.py
+++ NumberTheory/modulo.py
@@ -1,14 +1,14 @@
 import math
 
-__all__=['leg_sym','modular_sqrt','inverse','ext_euc','chin']
+__all__ = ['leg_sym', 'modular_sqrt', 'inverse', 'ext_euc', 'chin']
 
-def leg_sym(a,p):
-	ls=pow(a, (p - 1) // 2 ,p)
-	if ls == p-1:
-		return -1
-	else:
-		return ls
 
+def leg_sym(a, p):
+    ls = pow(a, (p - 1) // 2, p)
+    if ls == p-1:
+        return -1
+    else:
+        return ls
 
 
 def modular_sqrt(a, p):
@@ -58,40 +58,44 @@ def modular_sqrt(a, p):
         b = (b * g) % p
         r = m
 
-def inverse(a,n):
-	t=0
-	nt=1
-	r=n
-	nr=a
-	while nr != 0:
-		q = r//nr
-		r,nr = [nr,r - q * nr]
-		t,nt = [nt,t - q * nt]
-	if r > 1:
-		return 0
-	if t < 0:
-		t=t+n
-	return t
-def ext_euc(a,b):
-	ol_r, r = [a,b]
-	ol_s, s = [1,0]
-	while r != 0:
-		q = ol_r // r
-		ol_r,r = [r,ol_r - q * r]
-		ol_s,s = [s,ol_s - q * s]
-	if b != 0:
-		bez_t=(ol_r - ol_s * a) // b
-	else:
-		bez_t=0
-	return [ol_r,ol_s,bez_t]
 
-def chin(M,ytab,ntab):
-	tab=[]
-	for i in ntab:
-		Mi = M // i
-		gi = ext_euc(i,Mi)[2]
-		tab.append(Mi * gi)
-	wyn=0
-	for i in range(0,len(ytab)):
-		wyn+=tab[i]*ytab[i]
-	return wyn % M
+def inverse(a, n):
+    t = 0
+    nt = 1
+    r = n
+    nr = a
+    while nr != 0:
+        q = r//nr
+        r, nr = [nr, r - q * nr]
+        t, nt = [nt, t - q * nt]
+    if r > 1:
+        return 0
+    if t < 0:
+        t = t+n
+    return t
+
+
+def ext_euc(a, b):
+    ol_r, r = [a, b]
+    ol_s, s = [1, 0]
+    while r != 0:
+        q = ol_r // r
+        ol_r, r = [r, ol_r - q * r]
+        ol_s, s = [s, ol_s - q * s]
+    if b != 0:
+        bez_t = (ol_r - ol_s * a) // b
+    else:
+        bez_t = 0
+    return [ol_r, ol_s, bez_t]
+
+
+def chin(M, ytab, ntab):
+    tab = []
+    for i in ntab:
+        Mi = M // i
+        gi = ext_euc(i, Mi)[2]
+        tab.append(Mi * gi)
+    wyn = 0
+    for i in range(0, len(ytab)):
+        wyn += tab[i]*ytab[i]
+    return wyn % M
